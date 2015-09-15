686e9fe93f5bd2799332cdbcd2454e47ce70bfcb
Travis <travis@travis-ci.org>
2015-09-15 20:22:18 +0000

    Travis build 331 pushed to gh-pages

--- search/classes_70.js
+++ search/classes_70.js
@@ -3,7 +3,6 @@ var searchData=
   ['page',['Page',['../classLauncher_1_1Page.html',1,'Launcher']]],
   ['pal',['Pal',['../classCel_1_1Pal.html',1,'Cel']]],
   ['player',['Player',['../classFAWorld_1_1Player.html',1,'FAWorld']]],
-  ['playerfactory',['PlayerFactory',['../classFAWorld_1_1PlayerFactory.html',1,'FAWorld']]],
   ['playpage',['PlayPage',['../classLauncher_1_1PlayPage.html',1,'Launcher']]],
   ['position',['Position',['../classFAWorld_1_1Position.html',1,'FAWorld']]],
   ['posnetdata',['PosNetData',['../structFAWorld_1_1PosNetData.html',1,'FAWorld']]],
